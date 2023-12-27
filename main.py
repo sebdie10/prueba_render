60a99607566783f640bc1284fe6d831dbb8bf760
sebdie10 <diegoelgaray@gmail.com>
2023-12-26 21:09:50 -0300

    arreglo

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import flask
 
 
-def crear_app():
-    app= flask.Flask(__name__)
+app = flask.Flask(__name__)
 
 
-    @app.route('/')
-    def index():
-        return flask.render_template('index.html')
-    return app
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
 
 
 if __name__ == '__main__':
-    app = crear_app()
     app.run(debug=True)
