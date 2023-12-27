d8fb0eb21d03c72ace3ed2de466666a8ba94af6a
sebdie10 <diegoelgaray@gmail.com>
2023-12-26 21:36:23 -0300

    crear app

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import flask
 
+def crar_app():
+    app = flask.Flask(__name__)
 
-app = flask.Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
-
+    @app.route('/')
+    def index():
+        return flask.render_template('index.html')
+    
+    return app
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app = crar_app()
+    app.run()
